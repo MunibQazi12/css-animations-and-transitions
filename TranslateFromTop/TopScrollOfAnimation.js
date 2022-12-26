591b7c13ba059a0618cd13b310c1cd873ef3ad39
MunibQazi <munib@dataltics.com>
2022-12-26 19:38:57 +0500

    CSS animations and transitions are added

---
+++ TranslateFromTop/TopScrollOfAnimation.js
@@ -0,0 +1,15 @@
+
+const eventListener = (className) => {
+    const element = document.querySelectorAll(`${className}`);
+    for (var i = 0; i < element.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = element[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            element[i].classList.add("active");
+        } else {
+            element[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", () => { eventListener('.TopAnimation') }); 
